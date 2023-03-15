81cd7a10ce6a350ed0107d170bcd99fdb552e168
emobilis <lawrieabuna@gmail.com>
2023-03-15 08:53:38 +0300

    data types in git

--- src/main/kotlin/DataTypes.kt
+++ src/main/kotlin/DataTypes.kt
@@ -4,4 +4,22 @@ fun main(args: Array<String>) {
 
     //Strings
     var name = "John Doe"
+
+    //Chars - single letter and single quote
+    var city = 'K'
+
+    //Booleans
+    var status = true
+    var status = false
+
+    //Double - with multiple decimals
+    var temp = 36.54542
+
+    //Float - with a decimal
+    var lat:Float = 36.7F
+    // to convert a double to a float add F at the end of the data
+    // eg var demo = 25.56F
+
+
+
 }
