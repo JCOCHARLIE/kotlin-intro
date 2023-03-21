feb8825ff1c04fabebaa9f8b5d798952a9c330a1
emobilis <lawrieabuna@gmail.com>
2023-03-21 10:02:03 +0300

    functions

---
+++ src/main/kotlin/ex-function.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    addition(300, 500)
+    multiply(200,10)
+    divide(100, 5)
+}
+
+fun addition(num1:Int, num2:Int){
+    var result = num1+num2
+    println(result)
+}
+
+fun multiply(num3:Int, num4:Int){
+    var result = num3*num4
+    println(result)
+}
+
+fun divide(num5:Int, num6:Int){
+    var result = num5/num6
+    println(result)
+}
